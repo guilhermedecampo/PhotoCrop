a8722057b962856df05bfb871131f469cff7230a
Roman A. Sarria <agustin478@gmail.com>
2014-12-23 18:02:50 -0300

    Change of behaviour

--- www/PhotoCrop.js
+++ www/PhotoCrop.js
@@ -1,33 +1,10 @@
 /**
- * PhotoCrop is a modification of Cordova Camera Library for Android and iOS with Cropping utility
+ * PhotoCrop is an addition to the Cordova Camera Library for Android and iOS with Cropping utility
  */
-
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
-*/
 
 var argscheck = require('cordova/argscheck'),
     exec = require('cordova/exec'),
     PhotoCrop = require('./Camera');
-    // XXX: commented out
-    //CameraPopoverHandle = require('./CameraPopoverHandle');
 
 var cameraExport = {};
 
@@ -51,28 +28,15 @@ cameraExport.getPicture = function(successCallback, errorCallback, options) {
     options = options || {};
     var getValue = argscheck.getValue;
 
-    var quality = getValue(options.quality, 50);
+    var fileUri = getValue(options.fileUri, '');
     var destinationType = getValue(options.destinationType, PhotoCrop.DestinationType.FILE_URI);
-    var sourceType = getValue(options.sourceType, PhotoCrop.PictureSourceType.CAMERA);
-    var targetWidth = getValue(options.targetWidth, -1);
-    var targetHeight = getValue(options.targetHeight, -1);
-    var encodingType = getValue(options.encodingType, PhotoCrop.EncodingType.JPEG);
-    var mediaType = getValue(options.mediaType, PhotoCrop.MediaType.PICTURE);
-    var allowEdit = !!options.allowEdit;
-    var correctOrientation = !!options.correctOrientation;
-    var saveToPhotoAlbum = !!options.saveToPhotoAlbum;
-    var popoverOptions = getValue(options.popoverOptions, null);
-    var cameraDirection = getValue(options.cameraDirection, PhotoCrop.Direction.BACK);
-    
-    // allowEdit must be true
+
     var cropType = getValue(options.cropType, PhotoCrop.CropType.SQUARE);
 
     var args = [quality, destinationType, sourceType, targetWidth, targetHeight, encodingType,
                 mediaType, allowEdit, correctOrientation, saveToPhotoAlbum, popoverOptions, cameraDirection, cropType];
 
     exec(successCallback, errorCallback, "PhotoCrop", "takePicture", args);
-    // XXX: commented out
-    //return new CameraPopoverHandle();
 };
 
 cameraExport.cleanup = function(successCallback, errorCallback) {
